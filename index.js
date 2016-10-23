5de1471ace7439ca21858c8a21d4e4f8bae2566f
Vladimir Starkov <iamstarkov@gmail.com>
2016-10-23 03:06:13 +0200

    refactoring

--- index.js
+++ index.js
@@ -15,53 +15,53 @@ const findAccelerators = keymap => inputCommand => {
   return R.toPairs(keymap).reduce(acceleratorsReducerBy(inputCommand) , []);
 }
 
-const decorateMenu = menus => {
+const bindAccelerators = item => {
+  const { command } = item;
+  if (!command) {
+    return item;
+  }
   const findUserAccelerators = findAccelerators(userKeymap);
   const findDefaultAccelerators = findAccelerators(defaultKeymap);
 
-  const bindAccelerators = item => {
-    const { command } = item;
-    if (!command) {
-      return item;
-    }
-    const [userAccelerator,] = findUserAccelerators(command);
-    const [defaultAccelerator,] = findDefaultAccelerators(command);
+  const [userAccelerator,] = findUserAccelerators(command);
+  const [defaultAccelerator,] = findDefaultAccelerators(command);
 
-    const accelerator = userAccelerator ? userAccelerator : defaultAccelerator;
-    return R.merge(item, { accelerator });
-  };
+  const accelerator = userAccelerator ? userAccelerator : defaultAccelerator;
+  return R.merge(item, { accelerator });
+};
 
-  const rebindConflictedAccelerators = item => {
-    const { accelerator, command } = item;
-    if (!command) {
-      return item;
-    }
-
-    const userAccelerators = R.keys(userKeymap);
-    const userCommands = R.values(userKeymap);
-    const didUserUseThisAccelerator = userAccelerators.includes(accelerator);
-    const didUserRedefineCommand = userCommands.includes(command);
-    const isConflicted = didUserUseThisAccelerator && !didUserRedefineCommand;
-
-    if (!isConflicted) {
-      return item;
-    }
-
-    const defaultAccelerators = findDefaultAccelerators(command);
-    const defaultAcceleratorsWithoutUserOnes = R.without(userAccelerators, defaultAccelerators);
-    const [newAccelerator,] = defaultAcceleratorsWithoutUserOnes;
-
-    return R.merge(item, { accelerator: newAccelerator });
+const rebindConflictedAccelerators = item => {
+  const { accelerator, command } = item;
+  if (!command) {
+    return item;
   }
 
-  return menus.map(R.over(
-    R.lensProp('submenu'),
-    R.map(R.pipe(
-      bindAccelerators,
-      rebindConflictedAccelerators
-    ))
-  ));
+  const userAccelerators = R.keys(userKeymap);
+  const userCommands = R.values(userKeymap);
+  const didUserUseThisAccelerator = userAccelerators.includes(accelerator);
+  const didUserRedefineCommand = userCommands.includes(command);
+  const isConflicted = didUserUseThisAccelerator && !didUserRedefineCommand;
+
+  if (!isConflicted) {
+    return item;
+  }
+
+  const findDefaultAccelerators = findAccelerators(defaultKeymap);
+
+  const defaultAccelerators = findDefaultAccelerators(command);
+  const defaultAcceleratorsWithoutUserOnes = R.without(userAccelerators, defaultAccelerators);
+  const [newAccelerator,] = defaultAcceleratorsWithoutUserOnes;
+
+  return R.merge(item, { accelerator: newAccelerator });
 }
+
+const decorateMenu = R.map(R.over(
+  R.lensProp('submenu'),
+  R.map(R.pipe(
+    bindAccelerators,
+    rebindConflictedAccelerators
+  ))
+));
 
 const id = x => x;
 
